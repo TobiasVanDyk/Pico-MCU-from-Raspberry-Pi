00028b359142ede02a744cb959da2c2a502f6829
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2021-04-13 18:25:15 +0200

    Update ssd1306_128x64_i2cPico.ino

--- OLEDArduino/ssd1306_128x64_i2cPico.ino
+++ OLEDArduino/ssd1306_128x64_i2cPico.ino
@@ -63,7 +63,7 @@ void setup() {
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;); // Don't proceed, loop (allmost) forever
   }
 
   // Show initial display buffer contents on the screen --
