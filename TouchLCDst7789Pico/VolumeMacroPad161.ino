f49924c05b50855e6231e4e2e7656e111413b638
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2022-09-18 18:09:24 +0200

    Update VolumeMacroPad161.ino

--- TouchLCDst7789Pico/VolumeMacroPad161.ino
+++ TouchLCDst7789Pico/VolumeMacroPad161.ino
@@ -56,7 +56,7 @@ If current Layout is L3 then S1 to S6 changed if Layout is L4 then T1 to T6 chan
 If current Layer is A then S1/T1-S6/T6 changed, layer B S7/T7-S12/T12 changed
 Send new Text for Key [M4] - start with 0 end with LF
 End text string with LF. To send numbers use RealTerm - see the picture below
-(*) Must still implement F1-F12, and simultaneous keys other than A-Z.a-x,modifiers
+(*) Must still implement F1-F12, and simultaneous keys other than modifiers + A-Z
 ***********************************************************************************************************************************/
 #include "Adafruit_TinyUSB.h"
 #include <SPI.h>
@@ -1480,7 +1480,7 @@ void SendBytes()
       status("Macro Type 1"); }
           
   if ((KeyBrdByte[0]>0x7F) && (KeyBrdByteNum<6))
-     {for (n = 0; n < KeyBrdByteNum; n++) {b = KeyBrdByte[n]; if ((b>=0x40) && (b<=0x7A)) b = b - 61; keycode[n] = b; }
+     {for (n = 0; n < KeyBrdByteNum; n++) {b = KeyBrdByte[n]; if ((b>=0x40) && (b<=0x5B)) b = b - 61; keycode[n] = b; }
           usb_hid.keyboardReport(RID_KEYBOARD, 0, keycode); delay(keydelay);
           usb_hid.keyboardRelease(RID_KEYBOARD);            delay(keydelay);  }
       /*    
@@ -1508,7 +1508,7 @@ void MakeStr(int button)
     //Serial.print(" number HEX values ");
 
     b = KeyBrdByte[KeyBrdByteNum];
-    if ((b>=0x40) && (b<=0x7A)) byte a = b - 61;
+    if ((b>=0x40) && (b<=0x5B)) byte a = b - 61;
 
     /*
     for (int n = 0; n <= KeyBrdByteNum; n++) 
