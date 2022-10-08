af5230d36ef7b362e38557f4a2a025e548a22538
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2022-10-08 07:45:09 +0200

    Update VolumeMacroPad3.ino

--- TouchLCDili9488Pico/VolumeMacroPad3.ino
+++ TouchLCDili9488Pico/VolumeMacroPad3.ino
@@ -6,7 +6,7 @@
 //                (3) Bodmer-Adafruit Keypad Keypad_240x320.ino
 //                    https://github.com/Bodmer/TFT_eSPI/examples/
 ///////////////////////////////////////////////////////////////////////////////////////////
-// Adapted by T van Dyk Oct 2022 for ILI9486 480x320 and Adafruit TinyUSB stack
+// Adapted by T van Dyk Oct 2022 for ILI9488 480x320 and Adafruit TinyUSB stack
 ///////////////////////////////////////////////////////////////////////////////////////////
 /************************************************************************************
  Adafruit invests time and resources providing this open source code, please support 
