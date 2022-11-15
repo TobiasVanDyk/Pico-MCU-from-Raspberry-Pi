8f8f36e92d1377a68b698660ade1944d4abf8237
Tobias van Dyk <TobiasVanDyk@outlook.com>
2022-11-15 17:41:21 +0200

    Add files via upload

---
+++ TouchLCDili9486Pico/strings12.h
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Escape sequences \\ = backslash \? = ? \t = tab \b = backspace \" or \'  \x0A = hexadecimal number (byte)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+char str1[] = "1";
+char str2[] = "12";
+char str3[] = "123";                            
+char str4[] = "124";
+char str5[] = "sfc /scannow\n";  
+char str6[] = "\n\r"; 
+char str7[] = "cmd\r";                             
+char str8[] = "winver\n";  
+char str9[] = "cmd";
+char str10[] = "Dism /Online /Cleanup-Image /RestoreHealth"; 
+char str11[] = "Dism.exe /online /Cleanup-Image /StartComponentCleanup";                             
+char str12[] = "Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase"; 
