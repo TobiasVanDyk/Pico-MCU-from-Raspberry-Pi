480d265bebe4d854a5c040e174078fa28c915ac9
Tobias van Dyk <TobiasVanDyk@outlook.com>
2022-11-23 16:32:58 +0200

    Add files via upload

---
+++ TouchLCDst7789Pico/strings24.h
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Escape sequences \\ = backslash \? = ? \t = tab \b = backspace \" or \'  \x0A = hexadecimal number (byte)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+char str1[] = "1";
+char str2[] = "12";
+char str3[] = "123";                            
+char str4[] = "1234";
+char str5[] = "sfc /scannow\n";  
+char str6[] = "\n\r"; 
+char str7[] = "cmd\r";                             
+char str8[] = "winver\n";  
+char str9[] = "cmd";
+char str10[] = "Dism /Online /Cleanup-Image /RestoreHealth"; 
+char str11[] = "Dism.exe /online /Cleanup-Image /StartComponentCleanup";                             
+char str12[] = "Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase"; 
+char str13[] = " ";
+char str14[] = "    ";
+char str15[] = "@gmail.com";                            
+char str16[] = "@outlook.com";
+char str17[] = "sfc /scannow\n";  
+char str18[] = "\n\r"; 
+char str19[] = "cmd\r";                             
+char str20[] = "winver\n";  
+char str21[] = "cmd";
+char str22[] = "Dism /Online /Cleanup-Image /RestoreHealth"; 
+char str23[] = "Dism.exe /online /Cleanup-Image /StartComponentCleanup";                             
+char str24[] = "Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase"; 
