b26c8f82638e7afbb951e237053b78c3726ab55d
Tobias van Dyk <TobiasVanDyk@outlook.com>
2023-04-17 12:20:40 +0200

    Add files via upload

--- TouchLCDst7789Pico/User_Setup.h
+++ TouchLCDst7789Pico/User_Setup.h
@@ -339,13 +339,13 @@
 // #define SPI_FREQUENCY   5000000
 // #define SPI_FREQUENCY  10000000
 // #define SPI_FREQUENCY  20000000
-#define SPI_FREQUENCY  27000000
+#define SPI_FREQUENCY     14000000
 // #define SPI_FREQUENCY  40000000
 // #define SPI_FREQUENCY  55000000 // STM32 SPI1 only (SPI2 maximum is 27MHz)
 // #define SPI_FREQUENCY  80000000
 
 // Optional reduced SPI frequency for reading TFT
-#define SPI_READ_FREQUENCY  20000000
+#define SPI_READ_FREQUENCY  10000000
 
 // The XPT2046 requires a lower SPI clock rate of 2.5MHz so we define that here:
 #define SPI_TOUCH_FREQUENCY  2500000
