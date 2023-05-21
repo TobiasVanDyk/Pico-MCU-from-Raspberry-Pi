1a8593102f19eea4cf2d02e54e005dbf02f439a2
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2023-05-21 20:16:16 +0200

    Update VolumeMacroPad17.ino

--- TouchLCDili9488Pico/VolumeMacroPad17.ino
+++ TouchLCDili9488Pico/VolumeMacroPad17.ino
@@ -2774,7 +2774,7 @@ void GetSysInfo(int Action) // Also save config
   if (SaveReadLinks) {if (!LittleFS.exists("SRLinks")) {char SRLinks[2]  = "1"; ChrPtr = SRLinks; DoFileStrings(true, "SRLinks",  ChrPtr); } }
       else LittleFS.remove("SRLinks"); 
   
-  Serial.println("Version: VolumeMacro93 GPL3 Tobias van Dyk March 2023");
+  Serial.println("Version: VolumeMacro17 GPL3 Tobias van Dyk May 2023");
   Serial.printf("CPU MHz: %d\n\r", fCPU);
   Serial.printf("FreeHeap: %d\n\r", fHeap);
   Serial.printf("UsedHeap: %d\n\r", uHeap);
