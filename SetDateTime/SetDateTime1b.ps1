2663d560a7aabb645ad98a37d9df9067cd35d88e
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2023-08-28 11:21:28 +0200

    Delete SetDateTime directory

--- SetDateTime/SetDateTime1b.ps1
+++
@@ -1,6 +0,0 @@
-Get-Date > c:\Temp\DATE.TXT
-$g = Get-Content c:\Temp\DATE.TXT
-$c = $a+$g+$b
-$port.open()
-$port.Write($c)
-$port.Close()
--- SetDateTime/SetDateTime2.ps1
+++
@@ -1,8 +0,0 @@
-$a = "<t"
-$b = ">"
-$t = Get-Date -UFormat "%y%m%d%w%H%M"
-$c = $a+$t+$b
-$port= new-Object System.IO.Ports.SerialPort COM6,9600,None,8,one
-$port.open()
-$port.Write($c)
-$port.Close()
