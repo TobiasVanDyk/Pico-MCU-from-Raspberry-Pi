2fa125c56172e8a677a19c9db9870f362c4e9b65
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2024-04-18 11:07:56 +0200

    Delete TouchMacroPadPico/MacropadFoobarPlaying directory

--- TouchMacroPadPico/MacropadFoobarPlaying/FoobarPlaying.ps1
+++
@@ -1,8 +0,0 @@
-$file_data = Get-Content c:\Temp\FOOBAR.TXT
-$a = "<m"
-$b = ">"
-$d = $a+$file_data+$b
-$port= new-Object System.IO.Ports.SerialPort COM6,9600,None,8,one
-$port.open()
-$port.Write($d)
-$port.Close()
